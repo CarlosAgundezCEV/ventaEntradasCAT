7d290f5d0ac8a495b6609ff853010c33b2fd2e5c
CarlosAgundezCEV <carlos_agundez_apps1ma1718@cev.com>
2018-12-11 22:36:12 +0100

    Initial Commit

---
+++ ventaEntradasCAT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ventaEntradasCAT
+//
+//  Created by wizO on 11/12/2018.
+//  Copyright © 2018 Carlos Agundez Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
